facf313aef13956a412b4bb915261a043aa7ac2c
Mateus Padovani <mateuspadovani@gmail.com>
2018-09-25 11:59:04 -0300

    Update UICurrencyTextField.swift

--- UICurrencyTextField/Sources/UICurrencyTextField.swift
+++ UICurrencyTextField/Sources/UICurrencyTextField.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class UICurrencyTextField: UITextField {
+open public class UICurrencyTextField: UITextField {
     
     private var numberFormatter = NumberFormatter()
     
